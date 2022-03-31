163ae14b19422c7ee9caeab9481e315917fa6b44
Matatov1989 <Matatov1989@gmail.com>
2022-03-31 16:57:05 +0300

    added the small CRUD app

---
+++ public/javaScript/initFirebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCDUrogZ0VZT1LnVnbCtwJWmBfDmZmFX_g",
+	authDomain: "tnopis.firebaseapp.com",
+	projectId: "tnopis",
+	storageBucket: "tnopis.appspot.com",
+	messagingSenderId: "203296237691",
+	appId: "1:203296237691:web:982c812a388e536fcc9ade",
+	measurementId: "G-N4L51H9RTL"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
